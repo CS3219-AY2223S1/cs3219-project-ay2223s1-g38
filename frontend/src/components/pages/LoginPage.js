a12c7b5b40d2f454a3bef92985c913185634f1ec
Kevin Foong <kevin9foong@gmail.com>
2022-09-04 01:52:50 +0800

    Add linting and packages

--- frontend/src/components/pages/LoginPage.js
+++ frontend/src/components/pages/LoginPage.js
@@ -69,7 +69,7 @@ const LoginPage = () => {
 						backgroundPosition: "center"
 					}}
 				/>
-				<Grid item borderRadius={8} xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+				<Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
 					<Box
 						sx={{
 							my: 8,
