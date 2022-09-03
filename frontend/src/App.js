7a9a9c178e0d0e0a9ed6105b937ac8ad45132a01
Kevin Foong <kevin9foong@gmail.com>
2022-09-03 22:32:05 +0800

    Set up linting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,23 @@
-import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
-import SignupPage from './components/SignupPage';
-import LoginPage from './components/LoginPage';
-import {Box} from "@mui/material";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignupPage from "./components/SignupPage";
+import LoginPage from "./components/LoginPage";
+import { Box } from "@mui/material";
 
-function App() {
-    return (
-        <div className="App">
-            <Box display={"flex"} flexDirection={"column"} padding={"4rem"}>
-                <Router>
-                    <Routes>
-                        <Route exact path="/" element={<Navigate replace to="/signup" />}></Route>
-                        <Route path="/signup" element={<SignupPage/>}/>
-                        <Route path="/login" element={<LoginPage/>}/>
-                    </Routes>
-                </Router>
-            </Box>
-        </div>
-    );
-}
+const App = () => {
+	return (
+		<div className="App">
+			<Box display={"flex"} flexDirection={"column"} padding={"4rem"}>
+				<Router>
+					<Routes>
+						<Route exact path="/"></Route>
+						<Route path="/signup" element={<SignupPage/>}/>
+						<Route path="/login" element={<LoginPage/>}/>
+					</Routes>
+				</Router>
+			</Box>
+		</div>
+	);
+};
 
 export default App;
