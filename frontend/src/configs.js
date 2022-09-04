3d14dc0e64cda4cd3154c617121c00f0730038f0
tau-bar <taufiq.b.ar@gmail.com>
2022-09-04 21:05:37 +0800

    Add eslint and prettier configurations

--- frontend/src/configs.js
+++ frontend/src/configs.js
@@ -1,10 +1,10 @@
-const URI_USER_SVC = process.env.URI_USER_SVC || 'http://localhost:8000'
+const URI_USER_SVC = process.env.URI_USER_SVC || 'http://localhost:8000';
 
-const PREFIX_USER_SVC = '/api/user'
+const PREFIX_USER_SVC = '/api/user';
 
-const CREATE_USER_SVC = '/createUser'
-const LOGIN_SVC = '/login'
+const CREATE_USER_SVC = '/createUser';
+const LOGIN_SVC = '/login';
 
-export const URL_USER_SVC = URI_USER_SVC + PREFIX_USER_SVC
-export const URL_CREATE_USER_SVC =  URI_USER_SVC + PREFIX_USER_SVC + CREATE_USER_SVC
-export const URL_LOGIN_SVC = URI_USER_SVC + PREFIX_USER_SVC + LOGIN_SVC
+export const URL_USER_SVC = URI_USER_SVC + PREFIX_USER_SVC;
+export const URL_CREATE_USER_SVC = URI_USER_SVC + PREFIX_USER_SVC + CREATE_USER_SVC;
+export const URL_LOGIN_SVC = URI_USER_SVC + PREFIX_USER_SVC + LOGIN_SVC;
