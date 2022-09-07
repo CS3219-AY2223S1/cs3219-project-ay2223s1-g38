e69bca3a5e21cb09dd4644cdb1e322babe789d14
Yu Zher <yzlim23@gmail.com>
2022-09-07 17:19:47 +0800

    Add eslint to matching and user services

--- user-service/index.js
+++ user-service/index.js
@@ -1,23 +1,23 @@
-import express from 'express';
-import cors from 'cors';
+import cors from "cors";
+import express from "express";
 
 const app = express();
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(cors()) // config cors so that front-end can use
-app.options('*', cors())
-import { createUser, login } from './controller/user-controller.js';
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors()); // config cors so that front-end can use
+app.options("*", cors());
+import { createUser, login } from "./controller/user-controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
 // Controller will contain all the User-defined Routes
-router.get('/', (_, res) => res.send('Hello World from user-service'))
-router.post('/createUser', createUser)
-router.post('/login', login)
+router.get("/", (_, res) => res.send("Hello World from user-service"));
+router.post("/createUser", createUser);
+router.post("/login", login);
 
-app.use('/api/user', router).all((_, res) => {
-    res.setHeader('content-type', 'application/json')
-    res.setHeader('Access-Control-Allow-Origin', '*')
-})
+app.use("/api/user", router).all((_, res) => {
+	res.setHeader("content-type", "application/json");
+	res.setHeader("Access-Control-Allow-Origin", "*");
+});
 
-app.listen(8000, () => console.log('user-service listening on port 8000'));
+app.listen(8000, () => console.log("user-service listening on port 8000"));
--- user-service/model/repository.js
+++ user-service/model/repository.js
@@ -1,26 +1,27 @@
-import UserModel from './user-model.js';
-import 'dotenv/config'
 
-//Set up mongoose connection
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
+import UserModel from "./user-model.js";
+import "dotenv/config";
+
+// eslint-disable-next-line no-undef
 let mongoDB = process.env.ENV == "PROD" ? process.env.DB_CLOUD_URI : process.env.DB_LOCAL_URI;
 
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true });
 
 let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 export async function createUser(params) { 
-  return new UserModel(params)
+	return new UserModel(params);
 }
 
 export async function login(params) {
-  let user = await db.collection('usermodels').findOne({username : params.username, password : params.password});
-  return user;
+	let user = await db.collection("usermodels").findOne({ username : params.username, password : params.password });
+	return user;
 }
 
 export async function isUserExists(username) {
-  let user = await db.collection('usermodels').findOne({username : username});
-  return (user != null);
+	let user = await db.collection("usermodels").findOne({ username : username });
+	return (user != null);
 }
