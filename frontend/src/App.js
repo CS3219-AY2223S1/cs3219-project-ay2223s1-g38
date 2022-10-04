393c1a2088f4a93c953de9b8802b398b4548dfd9
Kevin Foong <kevin9foong@gmail.com>
2022-10-04 14:19:40 +0800

    Add collab editor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import { Box } from "@mui/material";
 import { useSelector } from "react-redux";
+import firebase from "firebase/app"; 
+import firebaseConfig from "./services/firebaseConfig";
 
 import SignupPage from "./components/pages/SignupPage";
 import LoginPage from "./components/pages/LoginPage";
 import { selectIsUserLoggedIn } from "./features/user/userSlice";
 import HomePage from "./components/pages/HomePage";
+import CollabPage from "./components/pages/CollabPage";
 
 const App = () => {
 	const isUserLoggedIn = useSelector(selectIsUserLoggedIn);
+
+	// Initialize firebase once 
+	useEffect(() => {
+		console.log("config: " + firebaseConfig.appId);
+		if (!firebase.apps.length) {
+			firebase.initializeApp(firebaseConfig); 
+		} else {
+			firebase.app(); 
+		}
+	}, []);
 
 	return (
 		<div className="App">
@@ -21,10 +34,12 @@ const App = () => {
 							<Route exact path="/" element={<Navigate replace to="/login" />}/>
 							<Route path="/signup" element={<SignupPage/>} />
 							<Route path="/login" element={<LoginPage/>} />
+							<Route path="/collab" element={<CollabPage/>} />
 						</Routes>
 						: 
 						<Routes>
 							<Route path="/login" element={<HomePage/>} />
+							<Route path="/collab" element={<CollabPage/>} />
 						</Routes>
 					}
 				</Router>
