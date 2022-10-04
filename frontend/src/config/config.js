763fed1cbecdd0b5e0eb6acdad8cac97d93c27af
Yu Zher <yzlim23@gmail.com>
2022-10-05 00:55:49 +0800

    Remove GCP details

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
 const URI_USER_SVC = process.env.URI_USER_SVC || "http://localhost:8000";
 
 const PREFIX_USER_SVC = "/api/user";
@@ -11,11 +11,13 @@ export const URL_CREATE_USER_SVC =  URI_USER_SVC + PREFIX_USER_SVC + CREATE_USER
 export const URL_LOGIN_SVC = URI_USER_SVC + PREFIX_USER_SVC + LOGIN_SVC;
 
 export const firebaseConfig = {
-	apiKey: "AIzaSyCK_0eJ03LOYFjRfoMvwQ7ZfZBg5mTxQXA",
-	authDomain: "cs3219-algohike.firebaseapp.com",
-	projectId: "cs3219-algohike",
-	storageBucket: "cs3219-algohike.appspot.com",
-	messagingSenderId: "903233715776",
-	appId: "1:903233715776:web:af225c16214c2e773729a5",
-	measurementId: "G-E0TQJS6QEH"
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_DATABASE_URL,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID
 };
+
+export default firebaseConfig; 
