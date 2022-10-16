92235c340a88c987fe63b35f200ce8adb6a71d2d
Kevin Foong <kevin9foong@gmail.com>
2022-10-17 02:00:56 +0800

    Add simple chat with socketio

--- communication-service/index.js
+++ communication-service/index.js
@@ -1,5 +1,26 @@
+import http from "http";
+
 import express from "express";
+import { Server } from "socket.io";
 
 const app = express(); 
 
-app.listen(9000, () => console.log("communication-service is listening on port 9000"));
+const server = http.createServer(app); 
+const io = new Server(server); 
+
+const __dirname = "/Users/kevin9foong/Desktop/y3s1/cs3219/project/cs3219-project-ay2223s1-g38/communication-service";
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html"); 
+});
+
+io.on("connection", (socket) => {
+	console.log("A user connected"); 
+
+	socket.on("chat message", (msg) => {
+		io.emit("chat message", msg);
+	}); 
+});
+
+server.listen(9000, () => console.log("communication-server listening on port " + server.address().port)); 
+
